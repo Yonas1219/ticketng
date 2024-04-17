3107538fea1ef738301a466c1c05a65fee3291e5
Yonas Enchalew <yonasenchalew@gmail.com>
2024-04-17 07:56:25 +0300

    first commit

---
+++ auth/src/index.ts
@@ -0,0 +1,42 @@
+import express from "express";
+import "express-async-errors";
+import { json } from "body-parser";
+import mongoose from "mongoose";
+// routes
+import { currentUserRouter } from "./routes/current-user";
+import { signinRouter } from "./routes/signin";
+import { signupRouter } from "./routes/signup";
+import { signoutRouter } from "./routes/signout";
+
+//middlewares
+import { errorHandler } from "./middlewares/error-handler";
+import { NotFoundError } from "./errors/not-found-error";
+
+const app = express();
+app.use(json());
+
+app.use(currentUserRouter);
+app.use(signupRouter);
+app.use(signoutRouter);
+app.use(signinRouter);
+
+app.all("*", async (req, res) => {
+  throw new NotFoundError();
+});
+
+app.use(errorHandler);
+
+const start = async () => {
+  try {
+    await mongoose.connect('mogodb://auth-mongo-srv:27017/auth');
+    
+  } catch (error) {
+    console.log(error);
+  } 
+  app.listen(3000, () => {
+    console.log("listening on port 3000");
+  });
+}; 
+
+
+start();
